396f0afc42b2d40db31bfa4c64d5fbb1854adcde
岡西陽平 <okanishiyouhei@okanishiyouheinoMacBook-Air.local>
2025-01-05 21:09:11 +0900

    rss取得時、取得元をキーにした連想配列に記事情報を格納するように修正

--- scripts/rss_fetcher.py
+++ scripts/rss_fetcher.py
@@ -5,31 +5,48 @@ import feedparser
 import requests
 from exceptions import FetchError, ParseError, UnexpectedError
 
+from constant.rss_types import RSSTypes
+
 
 class ContentFetcher:
     def __init__(self, urls):
         self.urls = urls
 
+    def determine_rss_type(self, url):
+        if RSSTypes.NIKKEI in url:
+            return RSSTypes.NIKKEI
+        elif RSSTypes.ITMEDIA in url:
+            return RSSTypes.ITMEDIA
+        # 他の条件を追加可能
+        return None
+
     def fetch_entries(self):
-        all_entries = []
-        for url in self.urls:
-            try:
+        all_entries = {RSSTypes.NIKKEI: [], RSSTypes.ITMEDIA: []}
+        try:
+            for url in self.urls:
+                rss_type = self.determine_rss_type(url)
+                if not rss_type:
+                    logging.warning(f"Unknown RSS type for URL: {url}")
+                    continue
+
                 response = requests.get(url)
                 response.raise_for_status()
                 feed = feedparser.parse(response.content)
                 if feed.bozo:
                     raise ParseError(f"Failed to parse feed: {url}")
-                all_entries.extend(feed.entries)
-            except requests.exceptions.RequestException as e:
-                raise FetchError(f"Network error fetching RSS feed from {url}: {e}")
-            except ParseError as e:
-                raise ParseError(
-                    f"Error processing entry: {entry.title if 'title' in entry else 'Unknown'}: {e}"
-                )
-            except Exception as e:
-                raise UnexpectedError(
-                    f"Unexpected error fetching RSS feed from {url}: {e}"
-                )
+
+                for entry in feed.entries:
+                    all_entries[rss_type].append(entry)
+
+        except requests.exceptions.RequestException as e:
+            raise FetchError(f"Network error fetching RSS feed from {url}: {e}")
+        except ParseError as e:
+            raise ParseError(
+                f"Error processing entry: {entry.title if 'title' in entry else 'Unknown'}: {e}"
+            )
+        except Exception as e:
+            raise UnexpectedError(f"Unexpected error fetching RSS feed from {url}: {e}")
+
         return all_entries
 
     def filter_recent_entries(self, entries):
