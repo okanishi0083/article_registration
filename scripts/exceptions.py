c5e3b34def52cd5ffb85d1f49ae0538f8914906b
okanishi0083 <134946266+okanishi0083@users.noreply.github.com>
2025-01-06 01:58:02 +0900

    Feature/get article data (#7)
    * Initial commit
    
    * beautifulsoup4を導入。pythonファイルを用途毎に分割
    
    * rssを使って取得するように修正。ローカル環境のみdotenvを使うよう修正
    
    * 修正中
    
    * gitignore追加
    
    * gitignoreに追加、pycacheは含めない
    
    * pycache削除
    
    * vscode設定ファイルはgit管理対象外にする
    
    * 定数クラスの作成
    
    * __pycache__はgit管理の対象外
    
    * rss取得時、取得元をキーにした連想配列に記事情報を格納するように修正
    
    * _determine_rss_typeに修正
    
    * 一応動く
    
    ---------
    
    Co-authored-by: 岡西陽平 <okanishiyouhei@okanishiyouheinoMacBook-Air.local>

--- scripts/exceptions.py
+++ scripts/exceptions.py
@@ -1,19 +1,34 @@
 class FetchError(Exception):
     """Raised when there is an error fetching RSS feeds."""
+
     pass
+
 
 class ParseError(Exception):
     """Raised when there is an error parsing RSS feeds."""
+
     pass
+
 
 class NotionAPIError(Exception):
     """Raised when there is an error interacting with the Notion API."""
+
     pass
+
+
+class FetchExistingEntriesError(Exception):
+    """Raised when there is an FetchExistingEntriesError"""
+
+    pass
+
 
 class UnexpectedError(Exception):
     """Raised when there is an error interacting with the UnexpectedError."""
+
     pass
+
 
 class CompareAndUpdate(Exception):
     """Raised when there is an error interacting with the CompareAndUpdate."""
+
     pass
--- scripts/rss_fetcher.py
+++ scripts/rss_fetcher.py
@@ -5,50 +5,63 @@ import feedparser
 import requests
 from exceptions import FetchError, ParseError, UnexpectedError
 
+from constant.rss_types import RSSTypes
+
 
 class ContentFetcher:
     def __init__(self, urls):
         self.urls = urls
 
+    def _determine_rss_type(self, url):
+        if RSSTypes.NIKKEI in url:
+            return RSSTypes.NIKKEI
+        elif RSSTypes.ITMEDIA in url:
+            return RSSTypes.ITMEDIA
+        # 他の条件を追加可能
+        return None
+
     def fetch_entries(self):
-        all_entries = []
-        for url in self.urls:
-            try:
+        all_entries = RSSTypes.initialize_entries()
+        try:
+            for url in self.urls:
+                rss_type = self._determine_rss_type(url)
+                if not rss_type:
+                    logging.warning(f"Unknown RSS type for URL: {url}")
+                    continue
+
                 response = requests.get(url)
                 response.raise_for_status()
                 feed = feedparser.parse(response.content)
                 if feed.bozo:
                     raise ParseError(f"Failed to parse feed: {url}")
-                all_entries.extend(feed.entries)
-            except requests.exceptions.RequestException as e:
-                raise FetchError(f"Network error fetching RSS feed from {url}: {e}")
-            except ParseError as e:
-                raise ParseError(
-                    f"Error processing entry: {entry.title if 'title' in entry else 'Unknown'}: {e}"
-                )
-            except Exception as e:
-                raise UnexpectedError(
-                    f"Unexpected error fetching RSS feed from {url}: {e}"
-                )
+
+                for entry in feed.entries:
+                    all_entries[rss_type].append(entry)
+
+        except requests.exceptions.RequestException as e:
+            raise FetchError(f"Network error fetching RSS feed from {url}: {e}")
+        except ParseError as e:
+            raise ParseError(
+                f"Error processing entry: {entry.title if 'title' in entry else 'Unknown'}: {e}"
+            )
+        except Exception as e:
+            raise UnexpectedError(f"Unexpected error fetching RSS feed from {url}: {e}")
+
         return all_entries
 
-    def filter_recent_entries(self, entries):
-        recent_entries = []
-        cutoff_date = datetime.now() - timedelta(days=2)
-        for entry in entries:
-            try:
-                entry_date = datetime(*entry.published_parsed[:6])
-                if entry_date >= cutoff_date:
-                    recent_entries.append(
-                        {
-                            "title": entry.title,
-                            "url": entry.link,
-                            "date": entry_date.strftime("%Y-%m-%d"),
-                            "description": entry.description,
-                        }
-                    )
-            except Exception as e:
-                raise ParseError(
-                    f"Error processing entry: {entry.title if 'title' in entry else 'Unknown'}: {e}"
-                )
-        return recent_entries
+    def filter_recent_entries(self, entry, date_field, cutoff_date, field_mapping):
+        # cutoff_date = datetime.now() - timedelta(days=2)
+        try:
+            entry_date = datetime(*getattr(entry, date_field)[:6])
+            if entry_date >= cutoff_date:
+                return {
+                    "title": getattr(entry, field_mapping["title"]),
+                    "url": getattr(entry, field_mapping["url"]),
+                    "date": entry_date.strftime("%Y-%m-%d"),
+                    "description": getattr(entry, field_mapping["description"]),
+                }
+        except Exception as e:
+            raise ParseError(
+                f"Error processing entry: {entry.title if 'title' in entry else 'Unknown'}: {e}"
+            )
+        return None
