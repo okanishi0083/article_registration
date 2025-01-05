dbf0db0af2d432742e488a47a811248b69ed95bc
okanishi0083 <134946266+okanishi0083@users.noreply.github.com>
2025-01-05 15:53:56 +0900

    特集/記事データの取得（#1）
    * Initial commit
    
    * Initial commit
    
    * beautifulsoup4を導入。pythonファイルを用途毎に分割
    
    * rssを使って取得するように修正。ローカル環境のみdotenvを使うよう修正
    
    * CODEOWNERS設定ファイルの追加
    
    ---------
    
    Co-authored-by: 岡西陽平 <okanishiyouhei@okanishiyouheinoMacBook-Air.local>

---
+++ scripts/main.py
@@ -0,0 +1,61 @@
+import logging
+import os
+
+from scripts.exceptions import CompareAndUpdate, FetchError, NotionAPIError, ParseError
+from scripts.notion_handler import DataHandler
+from scripts.rss_fetcher import ContentFetcher
+
+# ログ設定
+logging.basicConfig(filename="application_errors.log", level=logging.ERROR)
+
+
+def main():
+
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        print("Running in GitHub Actions")
+    else:
+        from dotenv import load_dotenv
+
+        load_dotenv()
+
+    rss_urls = os.getenv("RSS_URLS").split(",")
+    database_id = os.getenv("NOTION_DATABASE_ID")
+    notion_api_key = os.getenv("NOTION_API_KEY")
+
+    fetcher = ContentFetcher(rss_urls)
+    handler = DataHandler(database_id, notion_api_key)
+
+    try:
+        rss_datas = fetcher.fetch_entries()
+        rss_entries = fetcher.filter_recent_entries(rss_datas)
+    except (FetchError, ParseError, UnexpectedError) as e:
+        logging.error(f"RSS processing error: {e}")
+        return
+
+    try:
+        existing_entries = handler.fetch_existing_entries()
+    except NotionAPIError as e:
+        logging.error(f"Notion API error: {e}")
+        return
+
+    try:
+        new_entries, updated_entries = handler.compare_and_update(rss_entries)
+    except CompareAndUpdate as e:
+        logging.error(f"Notion API error: {e}")
+        return
+
+    try:
+        for entry in new_entries:
+            handler.post_to_notion(entry)
+    except NotionAPIError as e:
+        logging.error(f"Error posting new entry to Notion: {e}")
+
+        try:
+            for entry in updated_entries:
+                handler.update_notion_entry(entry)
+        except NotionAPIError as e:
+            logging.error(f"Error updating entry in Notion: {e}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/notion_handler.py
@@ -0,0 +1,84 @@
+import logging
+
+import requests
+
+from scripts.exceptions import CompareAndUpdate, FetchError, NotionAPIError, ParseError
+
+logging.basicConfig(filename="notion_api_errors.log", level=logging.ERROR)
+
+
+class DataHandler:
+    def __init__(self, database_id, notion_api_key):
+        self.database_id = database_id
+        self.notion_api_key = notion_api_key
+        self.headers = {
+            "Authorization": f"Bearer {self.notion_api_key}",
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28",
+        }
+
+    def fetch_existing_entries(self):
+        url = f"https://api.notion.com/v1/databases/{self.database_id}/query"
+        try:
+            response = requests.post(url, headers=self.headers)
+            response.raise_for_status()
+            return response.json()["results"]
+        except requests.exceptions.RequestException as e:
+            raise FetchError(f"Failed to fetch existing entries: {e}")
+
+    def compare_and_update(self, rss_entries, existing_entries):
+        try:
+            existing_entries_dict = {
+                entry["properties"]["サイトのURL"]["url"]: entry
+                for entry in existing_entries
+            }
+            new_entries = []
+            updated_entries = []
+
+            for rss_entry in rss_entries:
+                existing = existing_entries_dict.get(rss_entry["url"])
+
+                if existing:
+                    existing_title = existing["properties"]["タイトル"]["title"][0][
+                        "plain_text"
+                    ]
+                    existing_description = existing["properties"]["内容"]["rich_text"][
+                        0
+                    ]["plain_text"]
+
+                    if (
+                        rss_entry["title"] != existing_title
+                        or rss_entry["description"] != existing_description
+                    ):
+                        updated_entries.append(rss_entry)
+                else:
+                    new_entries.append(rss_entry)
+
+            return new_entries, updated_entries
+        except:
+            raise ConnectionAbortedError(f"Failed to fetch existing entries: {e}")
+
+    def post_to_notion(self, entry):
+        url = "https://api.notion.com/v1/pages"
+        try:
+            json_data = {
+                "parent": {"database_id": self.database_id},
+                "properties": {
+                    "タイトル": {"title": [{"text": {"content": entry["title"]}}]},
+                    "作成日": {"date": {"start": entry["date"]}},
+                    "サイトのURL": {"url": entry["url"]},
+                    "内容": {
+                        "rich_text": [{"text": {"content": entry["description"]}}]
+                    },
+                },
+            }
+            response = requests.post(url, headers=self.headers, json=json_data)
+            response.raise_for_status()
+            print(f"Successfully posted: {entry['title']}")
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Failed to post data: {json_data}\nError: {e}")
+            print(f"Error occurred: {e}")
+
+    def update_notion_entry(self, entry):
+        # Implement the update logic here
+        pass
