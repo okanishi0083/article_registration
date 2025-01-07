d607ede8cf9b7b60e44e9a809d5ecefe85805e7b
okanishi0083 <134946266+okanishi0083@users.noreply.github.com>
2025-01-08 00:58:25 +0900

    Develop (#17)
    * developブランチを最新 (#11)
    
    * ワークフロー
    
    * Create scheduled_python_work2.yml
    
    * tesuto
    
    * Update scheduled_python_work.yml
    
    * Update scheduled_python_work.yml (#10)
    
    * Create sheduled_python_test.yml
    
    * Update sheduled_python_test.yml
    
    ---------
    
    Co-authored-by: 岡西陽平 <okanishiyouhei@okanishiyouheinoMacBook-Air.local>
    
    * マイナビからニュースを取り込めるように修正 (#15)
    
    Co-authored-by: 岡西陽平 <okanishiyouhei@okanishiyouheinoMacBook-Air.local>
    
    * Feature/check selected word (#16)
    
    * タイトル、本文で特定の文言がないものは処理をスキップ
    
    * 不要なファイルを削除
    
    ---------
    
    Co-authored-by: 岡西陽平 <okanishiyouhei@okanishiyouheinoMacBook-Air.local>
    
    ---------
    
    Co-authored-by: 岡西陽平 <okanishiyouhei@okanishiyouheinoMacBook-Air.local>

--- constant/rss_types.py
+++ constant/rss_types.py
@@ -1,8 +1,14 @@
 class RSSTypes:
     NIKKEI = "nikkei"
     ITMEDIA = "itmedia"
+    MYNABI = "mynavi"
 
     @classmethod
     def initialize_entries(cls):
         """Initializes a dictionary with RSS types as keys and empty lists as values."""
-        return {cls.NIKKEI: [], cls.ITMEDIA: []}
+        return {cls.NIKKEI: [], cls.ITMEDIA: [], cls.MYNABI: []}
+
+    @classmethod
+    def get_all_types(cls):
+        """Returns a list of all RSS types."""
+        return [cls.NIKKEI, cls.ITMEDIA, cls.MYNABI]
